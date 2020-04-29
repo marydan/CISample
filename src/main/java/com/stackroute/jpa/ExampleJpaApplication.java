5f04cfc24892d7430d51f1ab2a21301382ca9e1f
Mary Rani <mary.rani@stackroute.in>
2020-04-29 19:11:39 +0530

    first

---
+++ src/main/java/com/stackroute/jpa/ExampleJpaApplication.java
@@ -0,0 +1,14 @@
+package com.stackroute.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ExampleJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleJpaApplication.class, args);
+	}
+
+}
